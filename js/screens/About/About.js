453a740bd8838cd707ef84651102f86c2588bd74
Gavin Jin <gavinjin62@gmail.com>
2019-05-27 16:27:08 -0700

    Initial commit

---
+++ js/screens/About/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+import { styles } from "./styles";
+
+const About = ({ conductData }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}></Text>
+    </View>
+  );
+};
+
+export default About;
---
+++ js/screens/About/AboutContainer.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import About from "./About";
+import { Text } from "react-native";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+class AboutContainer extends Component {
+  render() {
+    return (
+      <Query query={GET_CONDUCT_ITEMS}>
+        {({ loading, data }) => {
+          if (loading || !data) return <Text>"Loading..."</Text>;
+          console.log(data);
+          return <About conductData={data} />;
+        }}
+      </Query>
+    );
+  }
+}
+
+const GET_CONDUCT_ITEMS = gql`
+  query {
+    allConducts(orderBy: order_ASC) {
+      id
+      description
+      title
+    }
+  }
+`;
+
+export default AboutContainer;
